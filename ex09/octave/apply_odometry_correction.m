33033abfa4890a38c5732d443f2568cec1f15e8d
Fabian Meyer <user8324@posteo.net>
2018-01-02 16:51:01 +0100

    Implement ex09

--- ex09/octave/apply_odometry_correction.m
+++ ex09/octave/apply_odometry_correction.m
@@ -2,9 +2,14 @@
 % by applying the bias term to each line of the measurements
 % X: 	3x3 matrix obtained by the calibration process
 % U: 	Nx3 matrix containing the odometry measurements
-% C:	Nx3 matrix containing the corrected odometry measurements	
+% C:	Nx3 matrix containing the corrected odometry measurements
 
 function C = apply_odometry_correction(X, U)
-  % TODO: compute the calibrated motion vector, try to vectorize
-
+    % TODO: compute the calibrated motion vector, try to vectorize
+    % N = size(U, 1);
+    % C = zeros(N, 3);
+    % for i=1:N
+    %     C(i,:) = (X * U(i,:)')';
+    % end
+    C = U * X';
 end
--- ex09/octave/ls_calibrate_odometry.m
+++ ex09/octave/ls_calibrate_odometry.m
@@ -8,16 +8,45 @@
 % X:	the calibration matrix
 % returns the correction matrix X
 function X = ls_calibrate_odometry(Z)
-  % initial solution (the identity transformation)
-  X = eye(3); 
+    N = size(Z, 1);
+    % max iterations
+    M = 100;
+    % Error is normal distributed: omega = sigma = identity
+    Omega = eye(3);
+    % initial solution (the identity transformation)
+    X = eye(3);
 
-  % TODO: initialize H and b of the linear system
-  
-  % TODO: loop through the measurements and update H and b
-  % You may call the functions error_function and jacobian, see below
-  % We assume that the information matrix is the identity.
+    U_act = Z(i, 1:3)';
+    U_est = Z(i, 4:6)';
 
-  % TODO: solve and update the solution
+    % error values
+    e = zeros(3, N);
+
+    for k=1:M
+        % init H and b with 0
+        H = zeros(9,9);
+        b = zeros(1,9);
+
+        % accumulate data from measurements
+        for i=1:N
+            e(:,i) = error_function(i, X, Z);
+            ei = e(:,i);
+            J = jacobian(i, Z);
+
+            b = b + ei' * Omega * ei;
+            H = H + J'  * Omega * J;
+        end
+        b = b';
+
+        % check if error is sufficiently small
+        if norm(e) <= 1e-3
+            break;
+        end
+
+        % calc newton step
+        pk = -inv(H) * b;
+        X = X + reshape(pk, [3,3]);
+    end
 end
 
 % this function computes the error of the i^th measurement in Z
@@ -28,7 +57,10 @@ end
 %       and then the motion reported by odometry
 % e:	the error of the ith measurement
 function e = error_function(i, X, Z)
-  % TODO compute the error of each measurement
+    U_act = Z(i, 1:3)';
+    U_est = Z(i, 4:6)';
+
+    e =  U_act - X * U_est;
 end
 
 % derivative of the error function for the ith measurement in Z
@@ -36,5 +68,10 @@ end
 % Z:	the measurement matrix
 % J:	the jacobian of the ith measurement
 function J = jacobian(i, Z)
-  % TODO compute the Jacobian
+    U_est = Z(i, 4:6);
+
+    J = zeros(3,9);
+    J(1, 1:3) = U_est;
+    J(2, 4:6) = U_est;
+    J(3, 7:9) = U_est;
 end
