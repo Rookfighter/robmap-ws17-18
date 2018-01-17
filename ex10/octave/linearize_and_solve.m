66151f2a2865bf0f4e8a1ea246401885df1c6aca
Fabian Meyer <user8324@posteo.net>
2018-01-17 10:15:52 +0100

    Fix indentation

--- ex10/octave/linearize_and_solve.m
+++ ex10/octave/linearize_and_solve.m
@@ -39,8 +39,7 @@ for eid = 1:length(g.edges)
     % B Jacobian wrt x2
     [e, A, B] = linearize_pose_pose_constraint(xi, xj, z);
 
-    % TODO: compute and add the term to H and b
-
+    % compute and add the term to H and b
     % Hii
     H(i:i+2,i:i+2) += A' * Omega * A;
     % Hij
@@ -56,36 +55,35 @@ for eid = 1:length(g.edges)
     b(j:j+2) += e' * Omega * B;
 
     if (needToAddPrior)
-      % TODO: add the prior for one pose of this edge
-      % This fixes one node to remain at its current location
-      H(1:3,1:3) += eye(3);
-      needToAddPrior = false;
+        % This fixes one node to remain at its current location
+        H(1:3,1:3) += eye(3);
+        needToAddPrior = false;
     end
 
   % pose-landmark constraint
-  elseif (strcmp(edge.type, 'L') != 0)
-    % edge.fromIdx and edge.toIdx describe the location of
-    % the first element of the pose and the landmark in the state vector
-    % You should use also this index when updating the elements
-    % of the H matrix and the vector b.
-    % edge.measurement is the measurement
-    % edge.information is the information matrix
-    x1 = g.x(edge.fromIdx:edge.fromIdx+2);  % the robot pose
-    x2 = g.x(edge.toIdx:edge.toIdx+1);      % the landmark
-    Omega = edge.information;
-    i = edge.fromIdx;
-    j = edge.toIdx;
+    elseif (strcmp(edge.type, 'L') != 0)
+        % edge.fromIdx and edge.toIdx describe the location of
+        % the first element of the pose and the landmark in the state vector
+        % You should use also this index when updating the elements
+        % of the H matrix and the vector b.
+        % edge.measurement is the measurement
+        % edge.information is the information matrix
+        x1 = g.x(edge.fromIdx:edge.fromIdx+2);  % the robot pose
+        x2 = g.x(edge.toIdx:edge.toIdx+1);      % the landmark
+        Omega = edge.information;
+        i = edge.fromIdx;
+        j = edge.toIdx;
 
-    % Computing the error and the Jacobians
-    % e the error vector
-    % A Jacobian wrt x1
-    % B Jacobian wrt x2
-    [e, A, B] = linearize_pose_landmark_constraint(x1, x2, edge.measurement);
+        % Computing the error and the Jacobians
+        % e the error vector
+        % A Jacobian wrt x1
+        % B Jacobian wrt x2
+        [e, A, B] = linearize_pose_landmark_constraint(x1, x2, edge.measurement);
 
-    % TODO: compute and add the term to H and b
+        % TODO: compute and add the term to H and b
 
 
-  end
+    end
 end
 
 disp('solving system');
